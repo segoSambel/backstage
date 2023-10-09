0720916a54367259f750e69468b8ca42ed37456c
segoSambel <habibhusain313@gmail.com>
2023-10-09 17:17:05 +0700

    Fix performance issue from inspect code

--- core/src/main/java/me/segosambel/backstage/core/ui/MovieAdapter.kt
+++ core/src/main/java/me/segosambel/backstage/core/ui/MovieAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import me.segosambel.backstage.core.R
@@ -16,9 +17,11 @@ class MovieAdapter(
     private var listData: List<Movie> = emptyList()
 
     fun setData(newListData: List<Movie>?) {
-        if (newListData == null) return
+        if (newListData.isNullOrEmpty()) return
+        val diffCallback = ItemDiffUtilCallback(listData, newListData)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
         listData = newListData
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -47,6 +50,23 @@ class MovieAdapter(
                 .load("https://image.tmdb.org/t/p/w300/${data.posterUrl}")
                 .into(itemView.findViewById(R.id.iv_poster))
             itemView.findViewById<TextView>(R.id.tv_title).text = data.title
+        }
+    }
+
+    private inner class ItemDiffUtilCallback(
+        private val oldItem: List<Movie>,
+        private val newItem: List<Movie>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldItem.size
+
+        override fun getNewListSize(): Int = newItem.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldItem[oldItemPosition].movieId==newItem[newItemPosition].movieId
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldItem[oldItemPosition]==newItem[newItemPosition]
         }
     }
 }
